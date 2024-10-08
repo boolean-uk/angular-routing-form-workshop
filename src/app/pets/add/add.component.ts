e49e6f7125e91869480cafcc766f4bd84470aa0e
uerbzr <nigel.sibbert@gmail.com>
2024-10-08 09:25:23 +0100

    added form

--- src/app/pets/add/add.component.ts
+++ src/app/pets/add/add.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PetsService } from '../pets.service';
+import { Pet } from '../models/pet';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrl: './add.component.css'
+  styleUrl: './add.component.css',
 })
 export class AddComponent {
+  petForm: FormGroup;
+  pservice: PetsService;
+  constructor(
+    private formBuilder: FormBuilder,
+    private readonly petService: PetsService
+  ) {
+    this.petForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      age: [0, Validators.required],
+    });
+    this.pservice = this.petService;
+  }
 
+  addPet(): void {
+    const newPet: Pet = {
+      id: 0,
+      name: this.petForm.value.name,
+      description: this.petForm.value.description,
+      age: this.petForm.value.age,
+    };
+    this.pservice.AddPet(newPet);
+    this.petForm.reset();
+  }
 }
