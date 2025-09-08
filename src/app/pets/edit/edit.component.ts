cea08ce6fc1557dba03b9dff5ab066fe44b72952
uerbzr <nigel.sibbert@gmail.com>
2025-09-08 14:54:24 +0100

    removed old bootstrap

--- src/app/pets/edit/edit.component.ts
+++ src/app/pets/edit/edit.component.ts
@@ -8,7 +8,11 @@ import { PetsService } from '../pets.service';
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css'],
+  styleUrls: [
+    './edit.component.css',
+    'node_modules/bootstrap/dist/css/bootstrap.min.css',
+    'src/styles.css',
+  ],
 })
 export class EditComponent implements OnInit {
   petForm: FormGroup;
